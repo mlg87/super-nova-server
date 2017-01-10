ca611ccae7560b279862a47e82704462b332d655
jazzdragon <climbinghobo@gmail.com>
2017-01-09 18:31:29 -0700

    let's make sure

--- src/server/db/connection.js
+++ src/server/db/connection.js
@@ -1,4 +1,5 @@
 const environment = process.env.NODE_ENV;
 console.log('password is:', process.env.DB_PASSWORD);
 const config = require('../../../knexfile.js')[environment];
+console.log('envenvenv::', environment);
 module.exports = require('knex')(config);
