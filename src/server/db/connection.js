18b82a9d5e658d6e5ee06c23c0e32ded15362001
jazzdragon <climbinghobo@gmail.com>
2017-01-09 18:00:10 -0700

    see if it prints

--- src/server/db/connection.js
+++ src/server/db/connection.js
@@ -1,3 +1,4 @@
 const environment = process.env.NODE_ENV;
+console.log('password is:', process.env.DB_PASSWORD);
 const config = require('../../../knexfile.js')[environment];
 module.exports = require('knex')(config);
