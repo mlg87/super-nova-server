8ca9b8e759f8fbeaea38fffd628b5757f2653e71
jazzdragon <climbinghobo@gmail.com>
2017-01-12 16:35:05 -0700

    added seed

---
+++ src/server/db/seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO clients(name, time_zone) VALUES('Grand Junction', 'US/Mountain');
+
+INSERT INTO categories(name, client_id) VALUES
+  ('winter sports')
+  ('climbing')
+  ('water sports')
+  ('hiking');
