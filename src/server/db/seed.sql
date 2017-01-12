9dbf9d722e4b16a762743dc7132e3455f50cbe5d
jazzdragon <climbinghobo@gmail.com>
2017-01-12 16:39:31 -0700

    reset squence

--- src/server/db/seed.sql
+++ src/server/db/seed.sql
@@ -1,4 +1,8 @@
+DELETE FROM clients;
+ALTER SEQUENCE seq RESTART WITH 1;
+UPDATE clients SET id=nextval('seq');
 INSERT INTO clients(name, time_zone) VALUES('Grand Junction', 'US/Mountain');
 
+DELETE FROM categories;
 INSERT INTO categories(name, client_id) VALUES
-  ('winter sports'), ('climbing'), ('water sports'), ('hiking');
+  ('winter sports', 1), ('climbing', 1), ('water sports', 1), ('hiking', 1);
