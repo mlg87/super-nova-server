551e49e3114a25c70459ffc93606551bfeee6f31
jazzdragon <climbinghobo@gmail.com>
2017-01-14 09:24:36 -0700

    crazies

--- src/server/db/seed.sql
+++ src/server/db/seed.sql
@@ -1,8 +1,23 @@
 DELETE FROM clients;
+-- reset the id field so it can be referenced by other tables
 ALTER SEQUENCE clients_id_seq RESTART WITH 1;
--- UPDATE clients SET id=nextval('seq');
 INSERT INTO clients(name, time_zone) VALUES('Grand Junction', 'US/Mountain');
 
 DELETE FROM categories;
+-- reset the id field so it can be referenced by other tables
+ALTER SEQUENCE categories_id_seq RESTART WITH 1;
 INSERT INTO categories(name, client_id) VALUES
   ('winter sports', 1), ('climbing', 1), ('water sports', 1), ('hiking', 1);
+
+DELETE FROM item_types;
+ALTER SEQUENCE item_types_id_seq RESTART WITH 1;
+INSERT INTO item_types(name, category_id, client_id) VALUES
+  ('kayak', 3, 1), ('climbing shoes', 2, 1), ('harness', 2, 1), ('backpack', 4, 1);
+
+DELETE FROM inventory;
+INSERT INTO inventory(item_type_id, size, model, client_id) VALUES
+  (1, NULL, NULL, 1), (2, '41', 'La Sportiva Miura', 1), (3, 'medium', 'Arctery`x', 1);
+
+DELETE FROM users;
+INSERT INTO users(username, password) VALUES
+  ('alon01', '$2a$10$IP64aD6GTDUQvtHznWjdKOPazCiFghrh9B3TZADOnVWIF/aaXCKSG')
--- src/server/db/tableCreation.sql
+++ src/server/db/tableCreation.sql
@@ -1,5 +1,5 @@
 -- import gen_random_uuid
-CREATE EXTENSION pgcrypto;
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TYPE address AS (
   street TEXT,
@@ -7,6 +7,8 @@ CREATE TYPE address AS (
   country VARCHAR(50),
   zip_code VARCHAR(10)
 );
+
+CREATE TYPE gender AS ENUM ("Men's", "Women's", "Kids");
 
 CREATE TABLE clients (
   id SERIAL PRIMARY KEY,
@@ -16,6 +18,7 @@ CREATE TABLE clients (
   updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+-- inventory categories, e.g. 'climbing'
 CREATE TABLE categories (
   id SERIAL PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
@@ -25,18 +28,92 @@ CREATE TABLE categories (
   UNIQUE(name, client_id)
 );
 
-CREATE TABLE inventory (
+-- list the types of sizes we have - shirt sizes, pants sizes, shoe sizes...
+CREATE TABLE size_types (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(60) NOT NULL
+  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  client_id INT REFERENCES clients(id) NOT NULL,
+  UNIQUE(name, client_id)
+)
+
+CREATE TABLE item_types (
   id SERIAL PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
+  -- general item type description.
   description TEXT,
-  sku VARCHAR(50) NOT NULL DEFAULT gen_random_uuid(),
   category_id INT REFERENCES categories(id),
+  size_type_id INT REFERENCES size_types(id),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  client_id INT REFERENCES clients(id) NOT NULL,
+  UNIQUE(name, client_id)
+);
 
+CREATE TABLE brands (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  client_id INT REFERENCES clients(id) NOT NULL,
+  UNIQUE(name, client_id)
+)
+
+CREATE TABLE models (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  brand_id INT REFERENCES brands(id),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  client_id INT REFERENCES clients(id) NOT NULL,
+  UNIQUE(name, brand_id, client_id)
+)
+
+CREATE TABLE sizes (
+  id SERIAL PRIMARY KEY,
+  size VARCHAR(40) NOT NULL,
+  size_type_id INT REFERENCES size_types(id)
+  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  client_id INT REFERENCES clients(id) NOT NULL,
+  UNIQUE(size, size_type_id, client_id)
+)
+
+CREATE TABLE tags (
+  id SERIAL PRIMARY KEY,
+  tag VARCHAR(100) NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  client_id INT REFERENCES clients(id) NOT NULL,
+  UNIQUE(tag, client_id)
+)
+
+CREATE TABLE inventory (
+  id SERIAL PRIMARY KEY,
+  item_type_id INT REFERENCES item_types(id),
+  -- item description, will inherit from the item type if null.
+  description TEXT,
+  sku VARCHAR(50) NOT NULL,
+  size_id INT REFERENCES sizes(id),
+  gender GENDER,
+  brand_id INT REFERENCES brands(id),
+  model_id INT REFERENCES models(id),
   created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
   client_id INT REFERENCES clients(id) NOT NULL,
   UNIQUE(sku, client_id)
 );
+
+CREATE TABLE join_tags_inv (
+  tag_id INT REFERENCES tags(id),
+  inv_id INT REFERENCES inventory(id),
+)
+
+CREATE TABLE join_brands_item_types (
+  brand_id INT REFERENCES brands(id),
+  item_type_id INT REFERENCES item_types(id),
+)
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
@@ -76,17 +153,61 @@ CREATE TABLE reservations (
   client_id INT REFERENCES clients(id) NOT NULL
 );
 
--- Below is a function and trigger to set the sku of an inventory item to the id
--- if it wasn't set.
--- CREATE OR REPLACE FUNCTION set_sku_to_id() RETURNS TRIGGER AS $$
---   BEGIN
---     UPDATE inventory SET sku=CONCAT('#', NEW.id) WHERE id=NEW.id;
---     RETURN NULL;
---   END;
--- $$ LANGUAGE plpgsql;
---
--- CREATE TRIGGER set_default_sku
---   AFTER INSERT ON inventory
---   FOR EACH ROW
---   WHEN (NEW.sku IS NULL)
---   EXECUTE PROCEDURE set_sku_to_id();
+
+-- Create a trigger function that takes no arguments.
+-- Trigger functions automatically have OLD, NEW records
+-- and TG_TABLE_NAME as well as others.
+CREATE OR REPLACE FUNCTION generate_sku()
+RETURNS TRIGGER AS $$
+
+ -- Declare the variables we'll be using.
+DECLARE
+  key TEXT;
+  found TEXT;
+BEGIN
+
+  -- This loop will probably only run once per call until we've generated
+  -- millions of ids.
+  LOOP
+
+    -- Generate our string bytes and re-encode as a base64 string.
+    key := encode(gen_random_bytes(6), 'base64');
+
+    -- Concat the generated key (safely quoted) with the generated query
+    -- and run it.
+    -- SELECT id FROM "test" WHERE id='blahblah' INTO found
+    -- Now "found" will be the duplicated id or NULL.
+    EXECUTE 'SELECT sku FROM inventory WHERE sku=' || quote_literal(key) INTO found;
+
+    -- Check to see if found is NULL.
+    -- If we checked to see if found = NULL it would always be FALSE
+    -- because (NULL = NULL) is always FALSE.
+    IF found IS NULL THEN
+
+      -- If we didn't find a collision then leave the LOOP.
+      EXIT;
+    END IF;
+
+    -- We haven't EXITed yet, so return to the top of the LOOP
+    -- and try again.
+  END LOOP;
+
+  -- NEW and OLD are available in TRIGGER PROCEDURES.
+  -- NEW is the mutated row that will actually be INSERTed.
+  -- We're replacing id, regardless of what it was before
+  -- with our key variable.
+  NEW.sku = key;
+
+  -- The RECORD returned here is what will actually be INSERTed,
+  -- or what the next trigger will get if there is one.
+  RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+-- If an INSERT contains multiple RECORDs, each one will call
+-- unique_short_id individually.
+CREATE TRIGGER gen_sku_if_null
+  BEFORE INSERT ON inventory
+  FOR EACH ROW
+  WHEN (NEW.sku IS NULL)
+  EXECUTE PROCEDURE generate_sku();
