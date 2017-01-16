838c6e0cb1fb7cc569049809b78773749c439624
jazzdragon <climbinghobo@gmail.com>
2017-01-15 18:23:42 -0700

    pretty cool

--- src/server/db/functions.sql
+++ src/server/db/functions.sql
@@ -1,7 +1,8 @@
 CREATE OR REPLACE FUNCTION search_in_inventory(TEXT)
 RETURNS TABLE (item_id INT, type VARCHAR(50), uuid VARCHAR(50), size VARCHAR(40), gender GENDER, model VARCHAR(100), brand VARCHAR(100))
 AS $$
-SELECT i.id as item_id, it.name as type, i.uuid, s.size, gender, m.name AS model, b.name AS brand
+
+SELECT DISTINCT i.id as item_id, it.name as type, i.uuid, s.size, gender, m.name AS model, b.name AS brand
 FROM inventory i
 JOIN item_types it ON i.item_type_id = it.id
 LEFT OUTER JOIN sizes s ON i.size_id = s.id
@@ -10,5 +11,5 @@ LEFT OUTER JOIN brands b ON m.brand_id = b.id
 LEFT OUTER JOIN join_tags_inventory jti ON jti.inventory_id = i.id
 LEFT OUTER JOIN tags t ON jti.tag_id = t.id
 
-WHERE m.name = $1 OR b.name = $1 OR it.name = $1 OR t.tag = $1;
+WHERE m.name || ' ' || b.name || ' ' || to_tsvector(it.name) || to_tsvector(t.tag) @@ to_tsquery($1);
 $$ LANGUAGE SQL;
