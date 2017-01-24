ee6b8d9cd441dfefbf16104d3c8103bd8b211ec1
jazzdragon <climbinghobo@gmail.com>
2017-01-24 08:32:35 -0700

    accept headers

--- src/server/routes/inventory.js
+++ src/server/routes/inventory.js
@@ -3,12 +3,11 @@ const router = express.Router();
 const knex = require('../db/connection');
 
 router.get('/search', (req, res) => {
-  return knex.raw('SELECT * FROM search_inventory(?, ?)', [req.query.searchTerm, null])
+  return knex.raw('SELECT * FROM search_inventory(?, ?)', [req.headers.search_terms, null])
   .then((result) => {
     res.status(200).json(result.rows);
   })
   .catch((err) => {
-    console.log(err);
     res.status(500).json({message: 'An error ocurred while fetching inventory.'});
   });
 });
