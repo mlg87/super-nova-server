41541ad40cfe5f0ad94ca1b65f19d121127ac50c
jazzdragon <climbinghobo@gmail.com>
2017-02-09 21:04:47 -0700

    logging an error

--- src/server/routes/inventory.js
+++ src/server/routes/inventory.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const knex = require('../db/connection');
 
 router.get('/search', (req, res) => {
-  console.log(req.headers);
-  const { search_terms, category_id } = req.headers
+  const { search_terms, category_id } = req.headers;
   return knex.raw('SELECT * FROM search_inventory(?, ?, ?)', [search_terms, category_id, null])
   .then((result) => {
     res.status(200).json(result.rows);
   })
   .catch((err) => {
+    console.error(err)
     res.status(500).json({message: 'An error ocurred while fetching inventory.'});
   });
 });
