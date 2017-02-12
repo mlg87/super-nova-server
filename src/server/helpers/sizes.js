c3423c28414bf0383a61c4c03e1450857b8ef042
Pete Jeffryes <pete@flowhub.co>
2017-02-12 09:45:29 -0700

    made a sizes helper file

---
+++ src/server/helpers/sizes.js
@@ -0,0 +1,12 @@
+const knex = require('../db/connection');
+
+module.exports = {
+  getWithItemType: (item_type_id) => {
+    return knex.raw(
+      `SELECT * FROM item_types AS i
+      INNER JOIN sizes ON i.size_type_id = sizes.size_type_id
+      WHERE i.id = ?`,
+      [item_type_id]
+    )
+  }
+}
--- src/server/routes/sizes.js
+++ src/server/routes/sizes.js
@@ -2,19 +2,14 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../db/connection');
 const authHelpers = require('../helpers/auth');
+const sizes = require('../helpers/sizes');
 
 router.all('*', authHelpers.checkAuthentication);
 
 router.get(`/item_type/:item_type_id`, (req, res, next) => {
   const item_type_id = req.params.item_type_id;
-  knex.raw(
-    `SELECT * FROM item_types AS i
-    INNER JOIN sizes ON i.size_type_id = sizes.size_type_id
-    WHERE i.id = ?`,
-    [item_type_id]
-  )
+  sizes.getWithItemType(item_type_id)
   .then((data) => {
-    console.log(data);
     res.status(200).json({ data: data.rows });
   })
   .catch((err) => {
