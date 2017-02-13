5b371391f303ed1129d989647ccbc23cb9a37b83
Pete Jeffryes <pete@flowhub.co>
2017-02-12 17:44:20 -0700

    progress on inventory

--- src/server/routes/inventory.js
+++ src/server/routes/inventory.js
@@ -4,13 +4,11 @@ const knex = require('../db/connection');
 
 router.get('/inventory/search', (req, res) => {
   const { search_terms, category_id } = req.headers;
-  console.log('args:', search_terms, category_id);
   return knex.raw('SELECT * FROM search_inventory(?, ?, ?)', [search_terms, +category_id, null])
   .then((result) => {
     res.status(200).json({data: result.rows});
   })
   .catch((err) => {
-    console.log(err);
     res.status(500).json({error: 'An error ocurred while fetching inventory.'});
   });
 });
