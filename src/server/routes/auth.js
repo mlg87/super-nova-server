616b302c586c39d1f140183ce16e990265a37d7f
Pete Jeffryes <pete@flowhub.co>
2017-02-26 17:00:27 -0700

    logs

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -4,6 +4,7 @@ const authHelpers = require('../helpers/auth');
 const knex = require('../db/connection');
 
 router.post('/register', (req, res)  => {
+  console.log('hererererer?');
   authHelpers.createUser(req)
   .then((user) => { return authHelpers.encodeToken(user[0]); })
   .then((token) => {
